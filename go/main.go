9987608e7a509f6a4f46833e7216cd0152a5c53f
Fernando Marca Magalhães <fernandomarca@hotmail.com>
2022-07-31 23:42:59 -0300

    cli feature finish

--- go/main.go
+++ go/main.go
@@ -1,18 +1,7 @@
 package main
 
-import (
-	"database/sql"
-
-	db2 "github.com/praxiscode/go-hexagonal/adapters/db"
-	"github.com/praxiscode/go-hexagonal/application"
-)
+import "github.com/praxiscode/go-hexagonal/cmd"
 
 func main() {
-	db, _ := sql.Open("sqlite3", "sqlite.db")
-	productDbAdapter := db2.NewProductDb(db)
-	productService := application.NewProductService(productDbAdapter)
-
-	product, _ := productService.Create("Product Exemplo", 30)
-	productService.Enable(product)
-
+	cmd.Execute()
 }
